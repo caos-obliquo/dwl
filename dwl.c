dc2bfeb41dc7678ac2495cb228b8bdd4ad1c662e
beto bettini <caos_obliquo@proton.me>
2026-08-16 14:27:08 -0300

    fix: restore window title in bar (was accidentally removed)

--- dwl.c
+++ dwl.c
@@ -1766,8 +1766,15 @@ drawbar(Monitor *m)
 	x = drwl_text(m->drw, x, 0, w, m->b.height, m->lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->b.width - (tw + x + traywidth)) > m->b.height) {
-		drwl_setscheme(m->drw, colors[SchemeNorm]);
-		drwl_rect(m->drw, x, 0, w, m->b.height, 1, 1);
+		if (c) {
+			drwl_setscheme(m->drw, colors[m == selmon ? SchemeSel : SchemeNorm]);
+			drwl_text(m->drw, x, 0, w, m->b.height, m->lrpad / 2, client_get_title(c), 0);
+			if (c && c->isfloating)
+				drwl_rect(m->drw, x + boxs, boxs, boxw, boxw, 0, 0);
+		} else {
+			drwl_setscheme(m->drw, colors[SchemeNorm]);
+			drwl_rect(m->drw, x, 0, w, m->b.height, 1, 1);
+		}
 	}
 
 	if (traywidth > 0) {
